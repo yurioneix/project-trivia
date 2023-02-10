8e5c32145f0a6b10b09553285f5919d01550e91e
LucasRosa53 <lucasrosa223@hotmail.com>
2023-02-10 20:41:37 -0300

    resolvendo requisito 6

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -2,12 +2,15 @@ import React from 'react';
 // import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import HeaderInfo from '../components/HeaderInfo';
+import Questions from '../components/Questions';
 
 class Game extends React.Component {
   render() {
+    const { history } = this.props;
     return (
       <div>
         <HeaderInfo />
+        <Questions history={ history } />
       </div>
     );
   }
