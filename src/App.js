1e15d39c34bdf4b74839f744c4bbd5ae094a9777
yurioneix <yurioneix18@gmail.com>
2023-02-14 20:59:15 -0300

    Requisito 11: resolvendo

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Login from './pages/Login';
 import './App.css';
 import Settings from './pages/Settings';
 import Game from './pages/Game';
+import Feeback from './pages/Feedback';
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
       <Route exact path="/" component={ Login } />
       <Route path="/settings" component={ Settings } />
       <Route path="/game" component={ Game } />
+      <Route path="/feedback" component={ Feeback } />
     </Switch>
   );
 }
---
+++ src/pages/Feedback.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Feeback extends Component {
+  render() {
+    return (
+      <div>
+        <p data-testid="feedback-text">Feedback</p>
+      </div>
+    );
+  }
+}
+
+export default Feeback;
